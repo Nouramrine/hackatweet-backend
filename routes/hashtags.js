e4fdc6095662925dd5eb120f70bad39098659744
Nour <nouramrine@gmail.com>
2025-10-10 12:47:43 +0200

    mise en place page Hashtag

--- routes/hashtags.js
+++ routes/hashtags.js
@@ -1,48 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Hashtag = require('../models/hashtags');
+const Hashtag = require("../models/hashtags");
 
 router.get("/all", async (req, res) => {
-  
-    try{
-        const hashtags = await Hashtag.find({});
-        res.json({hashtags : hashtags});
-    }catch(err){
-        res.json({ result: false, message: err.message });
-    }
+  try {
+    const hashtags = await Hashtag.find({});
+    res.json({ hashtags: hashtags });
+  } catch (err) {
+    res.json({ result: false, message: err.message });
+  }
 });
 
 router.get("/:tag", async (req, res) => {
+  const tag = req.params.tag.toLowerCase();
 
-    const tag = req.params.tag;
+  try {
+    const hashtag = await Hashtag.findOne({ tag }).populate({
+      path: "tweets",
+      populate: { path: "author likedBy" },
+    });
 
-    if(!tag){
-        return res.json({result: false, message: "Missing param tag!"});
+    if (!hashtag) {
+      return res.json({ result: true, hashtags: [] });
     }
-  
-    try{
-        const hashtags = await Hashtag.find({tag: tag});
-        res.json({hashtags : hashtags});
-    }catch(err){
-        res.json({ result: false, message: err.message });
-    }
+
+    res.json({ result: true, hashtags: [hashtag] });
+  } catch (err) {
+    res.json({ result: false, message: err.message });
+  }
 });
 
 router.get("/number/:tag", async (req, res) => {
+  const tag = req.params.tag;
 
-    const tag = req.params.tag;
+  if (!tag) {
+    return res.json({ result: false, message: "Missing param tag!" });
+  }
 
-    if(!tag){
-        return res.json({result: false, message: "Missing param tag!"});
-    }
-  
-    try{
-        const hashtags = await Hashtag.find({tag: tag});
-        res.json({result : true, number: hashtags.length});
-    }catch(err){
-        res.json({ result: false, message: err.message });
-    }
+  try {
+    const hashtags = await Hashtag.find({ tag: tag });
+    res.json({ result: true, number: hashtags.length });
+  } catch (err) {
+    res.json({ result: false, message: err.message });
+  }
 });
 
 module.exports = router;
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,148 +1,127 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Tweet = require('../models/tweets');
-const User = require('../models/users');
-const Hashtag = require('../models/hashtags');
-const {checkBody} = require("../modules/checkBody");
+const Tweet = require("../models/tweets");
+const User = require("../models/users");
+const Hashtag = require("../models/hashtags");
+const { checkBody } = require("../modules/checkBody");
 
 router.get("/all", async (req, res) => {
-  
-    try{
-        const tweets = await Tweet.find({}).populate('author').populate('likedBy');
-        res.json({result: true, tweets : tweets});
-    }catch(err){
-        res.json({ result: false, message: err.message });
-    }
+  try {
+    const tweets = await Tweet.find({}).populate("author");
+    res.json({ tweets: tweets });
+  } catch (err) {
+    res.json({ result: false, message: err.message });
+  }
 });
-
 
 router.post("/new", async (req, res) => {
+  const { username, message } = req.body;
 
-    const {username, message} = req.body;
+  if (!checkBody(req.body, ["username", "message"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
 
-    if(!checkBody(req.body, ["username", "message"])){
-        res.json({result: false, error: "Missing or empty fields"});
-        return;
+  try {
+    const author = await User.findOne({ username: username });
+
+    if (!author) {
+      res.json({ result: false, error: "User not found" });
+      return;
     }
 
-    try{
+    const newTweet = new Tweet({
+      author: author.id,
+      message: message,
+      date: new Date(),
+      likes: 0,
+    });
 
-        const author = await User.findOne({username : username});
+    await newTweet.save();
 
-        if(!author){
-            res.json({result: false, error: "User not found"});
-            return;
-        }    
+    // Check for hashtages and add them in bdd
+    const pattern = /#(\w+)/g;
+    const hashtags = message.match(pattern);
 
-        const newTweet = new Tweet({
-            author : author.id, 
-            message : message,
-            date: new Date(),
-            likes: 0
+    if (hashtags) {
+      // message contains hashtags
+      for (let tag of hashtags) {
+        const lowerTag = tag.slice(1).toLowerCase();
+
+        const hashtag = await Hashtag.findOne({ tag: lowerTag });
+
+        // hashtag doesn't exist yet - create it
+        if (!hashtag) {
+          const newHashtag = new Hashtag({
+            tag: lowerTag,
+            tweets: [newTweet._id],
+          });
+
+          await newHashtag.save();
+        } else {
+          await Hashtag.updateOne(
+            { tag: lowerTag },
+            { $addToSet: { tweets: newTweet._id } }
+          );
         }
-        );
-
-        await newTweet.save();
-
-        // Check for hashtages and add them in bdd
-        //const pattern = /#(\w+)/g;
-        const pattern = /#([\p{L}\p{N}_]+)/gu;
-        const hashtags = message.match(pattern);
-
-        if(hashtags){ // message contains hashtags
-            for(let tag of hashtags){
-
-                const lowerTag = tag.slice(1).toLowerCase();
-
-                const hashtag = await Hashtag.findOne({ tag: lowerTag });
-
-                // hashtag doesn't exist yet - create it
-                if(!hashtag){
-                    const newHashtag = new Hashtag({
-                        tag: lowerTag,
-                        tweets : [newTweet._id]
-                    });
-
-                    await newHashtag.save();
-                }
-                else{
-                    await Hashtag.updateOne({tag: lowerTag}, { $addToSet: { tweets: newTweet._id } } );
-                }
-            }
-        }
-
-
-        res.json({result: true, message : "New tweet created"});
-    }catch(err){
-        res.json({ result: false, message: err.message });
+      }
     }
+
+    res.json({ result: true, message: "New tweet created" });
+  } catch (err) {
+    res.json({ result: false, message: err.message });
+  }
 });
 
-router.put("/like/:id", async (req, res) =>{
+router.put("/like/:id", async (req, res) => {
+  const id = req.params.id;
 
-    const id = req.params.id;
+  if (!id) {
+    return res.json({ result: false, message: "Missing param id!" });
+  }
 
-    if(!id){
-        return res.json({result: false, message: "Missing param id!"});
+  if (!checkBody(req.body, ["username"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  try {
+    const tweet = await Tweet.findOne({ _id: id });
+    if (!tweet) return res.json({ result: false, error: "Tweet not found" });
+
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.json({ result: false, error: "User not found" });
+
+    if (tweet.likedBy.some((e) => (e._id = user._id))) {
+      // user has already liked the tweet
+      await Tweet.updateOne({ _id: id }, { $pull: { likedBy: user._id } });
+      console.log("unliked");
+    } else {
+      await Tweet.updateOne({ _id: id }, { $addToSet: { likedBy: user._id } });
+      console.log("liked");
     }
 
-    if(!checkBody(req.body, ["username"])){
-        res.json({result: false, error: "Missing or empty fields"});
-        return;
-    }
-
-    try{
-
-        const tweet = await Tweet.findOne({_id: id}).populate('likedBy');
-        if (!tweet) {
-            console.log("tweet not found");
-            return res.json({ result: false, error: "Tweet not found" });
-        }
-
-        const user = await User.findOne({username: req.body.username})
-        if (!user) return res.json({ result: false, error: "User not found" });
-
-
-        const hasLiked = tweet.likedBy.some(e => e._id.equals(user._id));
-
-        if(hasLiked){ // user has already liked the tweet
-            await Tweet.updateOne(
-                {_id : id}, 
-                { $pull: { likedBy: user._id } }
-            );
-            console.log("unliked by " + req.body.username);
-        }else{  
-            await Tweet.updateOne(
-                {_id : id}, 
-                { $addToSet: { likedBy: user._id } }
-            );
-            console.log("liked by " + req.body.username);
-        }
-
-        res.json({result: true});
-    }catch(err){
-        res.json({ result: false, message: err.message });
-    }
+    res.json({ result: true });
+  } catch (err) {
+    res.json({ result: false, message: err.message });
+  }
 });
 
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
 
-router.delete("/:id", async (req, res) =>{
+  if (!id) {
+    return res.json({ result: false, message: "Missing param id!" });
+  }
 
-    const id = req.params.id;
+  try {
+    await Tweet.deleteOne({ _id: id });
 
-    if(!id){
-        return res.json({result: false, message: "Missing param id!"});
-    }
-
-    try{
-        await Tweet.deleteOne({_id : id});
-
-        res.json({result: true, message: "Tweet deleted"});
-    }catch(err){
-        res.json({ result: false, message: err.message });
-    }
+    res.json({ result: true, message: "Tweet deleted" });
+  } catch (err) {
+    res.json({ result: false, message: err.message });
+  }
 });
-
 
 module.exports = router;
